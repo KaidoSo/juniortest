174f94469212743d6056aee800a88bba7dcb9f25
KaidoSo <kaido.soomlais@gmail.com>
2021-01-15 16:05:30 +0200

    first commit

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<head>
+    <title>JuniorTest</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background: red !important;
+        }
+        .brand-text{
+            color: red !important;
+        }
+    </style>
+</head>
+    <body class="grey lighten-4">
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="#" class="left brand-logo brand-text">Product List</a>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="#" class= "btn brand">ADD</a></li>
+                    <li><a href="#" class= "btn brand">MASS DELETE</a></li>
+                </ul>
+            </div>    
+        </nav>
